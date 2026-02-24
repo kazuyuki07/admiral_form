28b91925d963104ad70d28e44f49875579e91935
かずゆき <128217532+kazuyuki07@users.noreply.github.com>
2026-02-24 11:23:38 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from mjml import mjml_to_html
-import html
-
-
-with open('templates/form_email.mjml', 'r', encoding='utf-8') as form:
-
-    result = mjml_to_html(form)
-    print(html.escape(result.html))
