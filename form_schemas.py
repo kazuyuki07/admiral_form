942a0bf97738c54dfcd62d6e213f1fb791447846
kazuyuki07 <artpashin07@gmail.com>
2026-03-04 18:22:53 +0300

    changed fillials

--- form_schemas.py
+++ form_schemas.py
@@ -1,22 +1,23 @@
-from pydantic import BaseModel
-from pydantic_extra_types.phone_numbers import PhoneNumber
-from datetime import datetime
 import enum
+from datetime import datetime
 from typing import List
 
+from pydantic import BaseModel
+from pydantic_extra_types.phone_numbers import PhoneNumber
+
+
 class FillialOptions(str, enum.Enum):
-    f1 = "Коломенская" 
-    f2 = "Царицино"
-    
-    # f1 = "Затонная, 22"
-    # f2 = "Судостроительная, 48"
-    # f3 = "Судостроительная, 46"
-    # f4 = "Новинки, 8"
-    # f5 = "Спортивная, вл 2"
-    # f6 = "Судостроительная 32к3"
+    f1 = "Москва, Затонная, 22"
+    f2 = "Москва, Новинки, 8"
+    f3 = "Москва, Судостроительная улица, 46с1"
+    f4 = "Москва, Судостроительная, 48"
+    f5 = "Москва, Спортивная, вл 2"
+    f6 = "Москва, Судостроительная 32к3"
+
 
 class Fillials(BaseModel):
     option: List[str]
+
 
 class Form(BaseModel):
     full_name: str
@@ -24,6 +25,7 @@ class Form(BaseModel):
     parrents_full_name: str
     number_phone: PhoneNumber
     fillial: FillialOptions
+
 
 class EmailRequest(BaseModel):
     email_sender: str
